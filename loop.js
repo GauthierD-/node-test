b451ec84fe4dbb319ec548c8ab7330db7d2e9329
Gauthier Deroo <g.deroo@gmail.com>
2012-10-15 20:43:31 +0200

    update loop.js for everyone

--- loop.js
+++ loop.js
@@ -1,64 +1,26 @@
-////////////////////////////////////////////////////////////////////////////////
-//                                                                            //
-//  Loop execute pour la simulation des utilisateurs.                         //
-//  La simulation se fait avec socket.io-client.                              //
-//  mon socket client genere les evenements et le serveur reagit              //
-//  en consequence. Voir le fichier Beloola/www_dyn/web/js/connection.js      //
-//  pour voir tous les evenements qui peuvent etre rajoutes.                  //
-//                                                                            //
-////////////////////////////////////////////////////////////////////////////////
+/*
+ * Loop.js, this file is use for simulate user on your server. 
+ * Client is simulate with socket-io.client and use event.
+ *
+ */
 
 "use strict";
 var io = require('socket.io-client'); 
 var socket = io.connect('https://localhost:8333/');
-socket.on("latencyRecup", function(data){
-    var t2 = new Date().getTime();
-    var t1 = data.time;
-    console.log("Latence du serveur : " + t2-t1);
-});
-/*
-* Objet que athos recoit (user de test: Gauthier Deroo)
-* clientObject = {
-*       id:         109,
-*       userName:   'sna',
-*       firstName:  'Gauthier',
-*       step:       '3',
-*       cookie: '8lcnibggkbmk32ne2gde8tokh4'
-*   }
-*/
-
 var self = this;
 var mydata = {
-	id          : 109,
-    userName    : "sna",
-    firstName   : "Gauthier",
-    step        : "3",
-    cookie      : "stressing", // permet de ne pas s'identifier sur le serveur. (auth.js)
-    hostUserId  : 0,
-    worldid     : 0,
-    animNumber  : 1
+	id          : 1,
+    userName    : "innie",
+    firstName   : "minnie",
+    hack        : "stressing", // for me this, allow me to connect on my server 
 }
 
-//connection server
+//connect server
 process.nextTick(function() {
-    socket.emit('auth.connect', mydata);    
+    socket.emit('connect', mydata);    
 });
 
-// creation du monde
-process.nextTick(function() {
-    socket.emit('world.createworld', mydata);    
-});
-
-//createion Avatar
-process.nextTick(function() {
-    socket.emit('avatar.createavatar', mydata);    
-});
-    
-//animation avatar
-process.nextTick(function() {
-    socket.emit('avatar.animate', mydata);
-});    
-//Move avatar with quat and Vec3 
+//Move avatar 
 process.nextTick(function(){
     var orientation = {
         s   :   0.9108,
@@ -79,86 +41,46 @@ process.nextTick(function(){
     mydata.orientation = orientation;
     mydata.position = position;
     mydata.velocity = velocity;
-    socket.emit("avatar.move", mydata);
+    socket.emit("move", mydata);
 });
 
+// getMailUser
 process.nextTick(function() {
-    socket.emit("avatar.move", mydata);    
-});
-process.nextTick(function() {
-    socket.emit("avatar.move", mydata);    
-});
-process.nextTick(function() {
-    socket.emit("avatar.move", mydata);    
-});
-process.nextTick(function() {
-    socket.emit("avatar.move", mydata);    
-});
-process.nextTick(function() {
-    socket.emit("avatar.move", mydata);    
-});
-process.nextTick(function() {
-    socket.emit("avatar.move", mydata);    
+    socket.emit("mailsUser", mydata);    
 });
 
-//teleport avatarToHome
-process.nextTick(function() {
-    socket.emit("avatar.teleportToHome", mydata);    
-});
-
-//teleport avatar
-process.nextTick(function() {
-    socket.emit("avatar.teleport", mydata)    
-});
-
-// getFriends
-process.nextTick(function() {
-    socket.emit("friends.send", mydata);
-});
-
-// getConversation
-process.nextTick(function() {
-    socket.emit("mail.sendConversations", mydata);    
-});
-
-//GetOneMail
+// GetOneMail
 process.nextTick(function() {
     mydata.conversationId = 1;
-    socket.emit("mail.sendMails", mydata);
+    socket.emit("oneMail", mydata);
 });
 
-//ReadMail
+// Search Mail
 process.nextTick(function() {
-    socket.emit("mail.read", mydata);
-})
-
-// openchatnofriend
-process.nextTick(function() {
-    mydata.userid = 109;
-    socket.emit("chat.openchatnofriend", mydata);
-});
-
-//mailsearch
-process.nextTick(function() {
-    mydata.keyword = "toto";
-    socket.emit("mail.search", mydata);
+    mydata.keyword = "yumyum";
+    socket.emit("findMail", mydata);
 });
 
 //sendMail
 process.nextTick(function() {
     mydata.content = "blablablabla";
     mydata.previous = 0;
-    mydata.usertoArray = [109];
-    socket.emit("mail.transmit", mydata);
+    mydata.usertoArray = [2];
+    socket.emit("sendMail", mydata);
 });
-
+/* Only For me =)
+ *
 process.nextTick(function() {
     var latencyPacket = {};
     latencyPacket.data = "azertyuiopqsdfghjklmwxcvbn";
     latencyPacket.time = new Date().getTime();
 
-    socket.emit("latencySend", latencyPacket);
+    socket.emit("latency", latencyPacket);
 });
+ *
+ */
+
+// etc ... 
 
 process.nextTick(function() {
     socket.emit("disconnect");
